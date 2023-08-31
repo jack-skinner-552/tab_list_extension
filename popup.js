7b1bca9388e47eba28fe289fa9541fd8a592f136
jack-skinner-552 <jack.skinner.552@gmail.com>
2023-08-31 13:10:37 -0600

    Add popup

---
+++ popup.js
@@ -0,0 +1,45 @@
+// popup.js
+
+function faviconURL(u) {
+  const url = new URL(chrome.runtime.getURL('/_favicon/'));
+  url.searchParams.set('pageUrl', u); // this encodes the URL as well
+  url.searchParams.set('size', '16');
+  return url.toString();
+}
+
+console.log("Popup script loaded");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const tabListElement = document.getElementById('tabList');
+
+  chrome.storage.local.get('tabList', function(result) {
+    const tabList = result.tabList || [];
+    const tabsByWindow = {};
+
+    tabList.forEach(tab => {
+      if (!tabsByWindow[tab.windowId]) {
+        tabsByWindow[tab.windowId] = [];
+      }
+      tabsByWindow[tab.windowId].push(tab);
+    });
+
+    for (const windowId in tabsByWindow) {
+      const windowTabs = tabsByWindow[windowId];
+      const windowElement = document.createElement('div');
+      const windowHeader = document.createElement('h3');
+      windowHeader.textContent = `Window ID: ${windowId}`;
+      windowElement.appendChild(windowHeader);
+
+      windowTabs.forEach(tab => {
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        img.src = faviconURL(tab.url);
+        li.appendChild(img);
+        li.appendChild(document.createTextNode(`Tab ID: ${tab.id}, URL: ${tab.url}`));
+        windowElement.appendChild(li);
+      });
+
+      tabListElement.appendChild(windowElement);
+    }
+  });
+});
